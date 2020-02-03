48d90fc94cf2d9434fa27d011aa898a6307c476c
Joey Vano <joseph@vano.io>
2020-02-03 11:31:01 -0500

    feat(contacts) add contacts screen

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React                    from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Contacts                 from "./screens/Contacts";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Contacts/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex           : 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/Contacts.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View
+}            from 'react-native';
+
+import ContactListItem from '../components/ContactListItem';
+import {fetchContacts} from "../utils/api";
+
+const keyExtractor = ({phone}) => phone;
+
+export default class Contacts extends React.Component {
+  state = {
+    contacts: [],
+    loading : true,
+    error   : false
+  };
+
+  async componentDidMount() {
+    try {
+      const contacts = await fetchContacts();
+
+      this.setState({
+        contacts,
+        loading: false,
+        error  : false
+      });
+    } catch (er) {
+      this.setState({
+        loading: false,
+        error  : true
+      });
+    }
+  }
+
+  renderContact = ({item}) => {
+    const {avatar, phone, name} = item;
+
+    return (
+      <ContactListItem avatar={avatar} phone={phone} name={name} onPress={() => console.log('Contact clicked!')}/>
+    );
+  };
+
+  render() {
+    const {contacts, loading, error} = this.state;
+
+    const contactsSorted = contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+    return (
+      <View style={styles.container}>
+        {loading && <ActivityIndicator size={'large'}/>}
+        {error && <Text style={styles.error}>Error ...</Text>}
+        {!loading && !error && (
+          <FlatList
+            data={contactsSorted}
+            keyExtractor={keyExtractor}
+            renderItem={this.renderContact}
+          />
+        )}
+      </View>
+    );
+
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    justifyContent : 'center',
+    flex           : 1
+  },
+  error    : {
+    color   : 'red',
+    fontSize: 18
+  }
+});
